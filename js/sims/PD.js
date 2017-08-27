e4606bef33090784490ef857415ce20957be4ac4
Shirish Pokharel <shirish.pokharel@tufts.edu>
2017-08-27 08:08:00 +0000

    almost ready to finish

--- js/sims/PD.js
+++ js/sims/PD.js
@@ -58,6 +58,9 @@ PD.playOneGame = function(playerA, playerB){
 	// Make your moves!
 	var A = playerA.play();
 	var B = playerB.play();
+	var A_mood = playerA.mood();
+	var B_mood = playerB.mood();
+
 
 	// Noise: random mistakes, flip around!
 	if(Math.random()<PD.NOISE) A = ((A==PD.COOPERATE) ? PD.CHEAT : PD.COOPERATE);
@@ -67,8 +70,8 @@ PD.playOneGame = function(playerA, playerB){
 	var payoffs = PD.getPayoffs(A,B);
 
 	// Remember own & other's moves (or mistakes)
-	playerA.remember(A, B);
-	playerB.remember(B, A);
+	playerA.remember(A, B_mood);
+	playerB.remember(B, A, A_mood);
 
 	// Add to scores (only in tournament?)
 	playerA.addPayoff(payoffs[0]);
@@ -121,79 +124,410 @@ PD.playOneTournament = function(agents, turns){
 
 };
 
+
+//
+//Extra code added by Shirish Pokharel
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+Number.prototype.map = function (in_min, in_max, out_min, out_max) {
+  return (this - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+function should_change(moodval){
+	var res = 0;
+	var rand_num = getRandomInt(0, 101);
+	if (moodval < 0){
+		res = Math.ceil(num.map(.1, 6, 0, 10));
+		if (rand_num <= res){
+			return PD.CHEAT;
+		}
+	}
+	if (moodval > 0){
+		res = Math.ceil(num.map(-10, -.1, 0, 20));
+		if (rand_num <= res){
+			return PD.COOPERATE;
+		}
+	}
+	return 0;
+}
+
+
+//
+
 ///////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////
 
 function Logic_tft(){
 	var self = this;
+	var mood = getRandomInt(-10, 6);
+	var happiness_counter = 0;
+	var anger_counter = 0;
+	var anger_agree = 0;
+	var happiness_disagree = 0;
+	var anger_influenced = false;
+	var happiness_influenced = false;
+	var next_move = 0;
+	
 	var otherMove = PD.COOPERATE;
+	
+	self.mood = function(){
+		return mood;
+	}
+
 	self.play = function(){
+		//If we are 5 moves away from having cheated a happy negotiator...
+		if (happiness_counter > 4){
+			happiness_counter = 0;
+			//if they admonished us by disagreeing more than 2/5 times
+			if (happiness_disagree > 2){
+				//we know not to be influened by the happiness rule anymore
+				//because they've discovered we're taking advantage of their
+				//happiness.
+				happiness_influenced = false;
+			}
+			happiness_disagree = 0;
+		}
+		//if we are 6 moves away from capitulating to an angry negotiator
+		if (anger_counter > 5){
+			anger_counter = 0;
+			//if they have not reciprocated at least 2/6 times...
+			if (anger_agree < 3){
+				//we know they are irrationally crazy, and won't return the
+				//favor even if we capitulate. So, ignore the anger rule.
+				anger_influenced = false;
+			}
+			anger_agree = 0;
+		}
+		
+		
+		
+		if (next_move != 0 && next_move != otherMove){
+			return next_move;
+		}
 		return otherMove;
 	};
-	self.remember = function(own, other){
+	self.remember = function(own, other, other_mood){
+		next_move = should_change(other_mood);
 		otherMove = other;
+		if (next_move == PD.CHEAT) happiness_counter++;
+		if (next_move == PD.COOPERATE) anger_counter++;
+
 	};
 }
 
 function Logic_tf2t(){
 	var self = this;
+	var mood = getRandomInt(-10, 6);
+	var happiness_counter = 0;
+	var anger_counter = 0;
+	var anger_agree = 0;
+	var happiness_disagree = 0;
+	var anger_influenced = false;
+	var happiness_influenced = false;
 	var howManyTimesCheated = 0;
+	var next_move = 0;
+
+	self.mood = function(){
+		return mood;
+	}
+
 	self.play = function(){
-		if(howManyTimesCheated>=2){
-			return PD.CHEAT; // retaliate ONLY after two betrayals
-		}else{
-			return PD.COOPERATE;
+
+		//If we are 5 moves away from having cheated a happy negotiator...
+		if (happiness_counter > 4){
+			happiness_counter = 0;
+			//if they admonished us by disagreeing more than 2/5 times
+			if (happiness_disagree > 2){
+				//we know not to be influened by the happiness rule anymore
+				//because they've discovered we're taking advantage of their
+				//happiness.
+				happiness_influenced = false;
+			}
+			happiness_disagree = 0;
 		}
+		//if we are 6 moves away from capitulating to an angry negotiator
+		if (anger_counter > 5){
+			anger_counter = 0;
+			//if they have not reciprocated at least 2/6 times...
+			if (anger_agree < 3){
+				//we know they are irrationally crazy, and won't return the
+				//favor even if we capitulate. So, ignore the anger rule.
+				anger_influenced = false;
+			}
+			anger_agree = 0;
+		}
+
+
+
+		var toReturn = 0;
+		
+		if(howManyTimesCheated>=2){
+			toReturn = PD.CHEAT; // retaliate ONLY after two betrayals
+		}else{
+			toReturn = PD.COOPERATE;
+		}
+		
+		if (next_move != 0 && next_move != toReturn){
+			return next_move;
+		}		
+		return toReturn;
 	};
-	self.remember = function(own, other){
+	self.remember = function(own, other, other_mood){
+		next_move = should_change(other_mood)
+		
 		if(other==PD.CHEAT){
 			howManyTimesCheated++;
 		}else{
 			howManyTimesCheated = 0;
 		}
+		
+		if (next_move == PD.CHEAT) happiness_counter++;
+		if (next_move == PD.COOPERATE) anger_counter++;
+
 	};
 }
 
 function Logic_grudge(){
 	var self = this;
 	var everCheatedMe = false;
+	var mood = getRandomInt(-10, 6);
+	var happiness_counter = 0;
+	var anger_counter = 0;
+	var anger_agree = 0;
+	var happiness_disagree = 0;
+	var anger_influenced = false;
+	var happiness_influenced = false;
+	var next_move = 0;
+
+	self.mood = function(){
+		return mood;
+	}
+
+
 	self.play = function(){
-		if(everCheatedMe) return PD.CHEAT;
-		return PD.COOPERATE;
+		//If we are 5 moves away from having cheated a happy negotiator...
+		if (happiness_counter > 4){
+			happiness_counter = 0;
+			//if they admonished us by disagreeing more than 2/5 times
+			if (happiness_disagree > 2){
+				//we know not to be influened by the happiness rule anymore
+				//because they've discovered we're taking advantage of their
+				//happiness.
+				happiness_influenced = false;
+			}
+			happiness_disagree = 0;
+		}
+		//if we are 6 moves away from capitulating to an angry negotiator
+		if (anger_counter > 5){
+			anger_counter = 0;
+			//if they have not reciprocated at least 2/6 times...
+			if (anger_agree < 3){
+				//we know they are irrationally crazy, and won't return the
+				//favor even if we capitulate. So, ignore the anger rule.
+				anger_influenced = false;
+			}
+			anger_agree = 0;
+		}
+		
+		var toReturn = 0;
+		if(everCheatedMe){
+			toReturn = PD.CHEAT;	
+		} 
+		else{
+			toReturn =  PD.COOPERATE;
+		}
+		
+		if (next_move != 0 && next_move != toReturn){
+			return next_move;
+		}		
+		return toReturn;
+
 	};
-	self.remember = function(own, other){
+	self.remember = function(own, other, other_mood){
+		next_move = should_change(other_mood);
+
 		if(other==PD.CHEAT) everCheatedMe=true;
+		
+		if (next_move == PD.CHEAT) happiness_counter++;
+		if (next_move == PD.COOPERATE) anger_counter++;
+
 	};
 }
 
 function Logic_all_d(){
 	var self = this;
+	var mood = getRandomInt(-10, 6);
+	var happiness_counter = 0;
+	var anger_counter = 0;
+	var anger_agree = 0;
+	var happiness_disagree = 0;
+	var anger_influenced = true;
+	var happiness_influenced = true;
+	var next_move = 0;
+	
+	
+	self.mood = function(){
+		return mood;
+	}
 	self.play = function(){
-		return PD.CHEAT;
+
+		//If we are 5 moves away from having cheated a happy negotiator...
+		if (happiness_counter > 4){
+			happiness_counter = 0;
+			//if they admonished us by disagreeing more than 2/5 times
+			if (happiness_disagree > 2){
+				//we know not to be influened by the happiness rule anymore
+				//because they've discovered we're taking advantage of their
+				//happiness.
+				happiness_influenced = false;
+			}
+			happiness_disagree = 0;
+		}
+		//if we are 6 moves away from capitulating to an angry negotiator
+		if (anger_counter > 5){
+			anger_counter = 0;
+			//if they have not reciprocated at least 2/6 times...
+			if (anger_agree < 3){
+				//we know they are irrationally crazy, and won't return the
+				//favor even if we capitulate. So, ignore the anger rule.
+				anger_influenced = false;
+			}
+			anger_agree = 0;
+		}
+
+		var toReturn = PD.CHEAT;
+		if (next_move != 0 && next_move != toReturn){
+			return next_move;
+		}		
+		return toReturn;
+
 	};
-	self.remember = function(own, other){
+	self.remember = function(own, other, other_mood){
 		// nah
+		next_move = should_change(other_mood);
+		if (next_move == PD.CHEAT) happiness_counter++;
+		if (next_move == PD.COOPERATE) anger_counter++;
+		if (happiness_counter > 0 && other == PD.CHEAT){
+			happiness_disagree++;
+		}
+		if (anger_counter > 0 && other == PD.COOPERATE){
+			anger_agree++;
+		}
+
 	};
 }
 
 function Logic_all_c(){
 	var self = this;
+	var mood = getRandomInt(-10, 6);
+	var happiness_counter = 0;
+	var anger_counter = 0;
+	var anger_agree = 0;
+	var happiness_disagree = 0;
+	var anger_influenced = false;
+	var happiness_influenced = false;
+	var next_move = 0;
+	
+	self.mood = function(){
+		return mood;
+	}
 	self.play = function(){
-		return PD.COOPERATE;
+		//If we are 5 moves away from having cheated a happy negotiator...
+		if (happiness_counter > 4){
+			happiness_counter = 0;
+			//if they admonished us by disagreeing more than 2/5 times
+			if (happiness_disagree > 2){
+				//we know not to be influened by the happiness rule anymore
+				//because they've discovered we're taking advantage of their
+				//happiness.
+				happiness_influenced = false;
+			}
+			happiness_disagree = 0;
+		}
+		//if we are 6 moves away from capitulating to an angry negotiator
+		if (anger_counter > 5){
+			anger_counter = 0;
+			//if they have not reciprocated at least 2/6 times...
+			if (anger_agree < 3){
+				//we know they are irrationally crazy, and won't return the
+				//favor even if we capitulate. So, ignore the anger rule.
+				anger_influenced = false;
+			}
+			anger_agree = 0;
+		}
+
+		toReturn = PD.COOPERATE;
+		if (next_move != 0 && next_move != toReturn){
+			return next_move;
+		}		
+		return toReturn;
+
 	};
-	self.remember = function(own, other){
+	self.remember = function(own, other, other_mood){
 		// nah
+		next_move = should_change(other_mood);
+		if (next_move == PD.CHEAT) happiness_counter++;
+		if (next_move == PD.COOPERATE) anger_counter++;
+
 	};
 }
 
 function Logic_random(){
 	var self = this;
+	var mood = getRandomInt(-10, 6);
+	var happiness_counter = 0;
+	var anger_counter = 0;
+	var anger_agree = 0;
+	var happiness_disagree = 0;
+	var anger_influenced = false;
+	var happiness_influenced = false;
+	var next_move = 0;
+	
+	self.mood = function(){
+		return mood;
+	}
 	self.play = function(){
-		return (Math.random()>0.5 ? PD.COOPERATE : PD.CHEAT);
+		//If we are 5 moves away from having cheated a happy negotiator...
+		if (happiness_counter > 4){
+			happiness_counter = 0;
+			//if they admonished us by disagreeing more than 2/5 times
+			if (happiness_disagree > 2){
+				//we know not to be influened by the happiness rule anymore
+				//because they've discovered we're taking advantage of their
+				//happiness.
+				happiness_influenced = false;
+			}
+			happiness_disagree = 0;
+		}
+		//if we are 6 moves away from capitulating to an angry negotiator
+		if (anger_counter > 5){
+			anger_counter = 0;
+			//if they have not reciprocated at least 2/6 times...
+			if (anger_agree < 3){
+				//we know they are irrationally crazy, and won't return the
+				//favor even if we capitulate. So, ignore the anger rule.
+				anger_influenced = false;
+			}
+			anger_agree = 0;
+		}
+		toReturn =  (Math.random()>0.5 ? PD.COOPERATE : PD.CHEAT);
+		if (next_move != 0 && next_move != toReturn){
+			return next_move;
+		}		
+		return toReturn;
+
 	};
-	self.remember = function(own, other){
+	self.remember = function(own, other, other_mood){
 		// nah
+		next_move = should_change(other_mood);
+		if (next_move == PD.CHEAT) happiness_counter++;
+		if (next_move == PD.COOPERATE) anger_counter++;
+
 	};
 }
 
@@ -201,13 +535,60 @@ function Logic_random(){
 // Then, if opponent cooperated, repeat past move. otherwise, switch.
 function Logic_pavlov(){
 	var self = this;
+	var mood = getRandomInt(-10, 6);
+	var happiness_counter = 0;
+	var anger_counter = 0;
+	var anger_agree = 0;
+	var happiness_disagree = 0;
+	var anger_influenced = false;
+	var happiness_influenced = false;
+	var next_move = 0;
+	
 	var myLastMove = PD.COOPERATE;
+	
+	self.mood = function(){
+		return mood;
+	}	
 	self.play = function(){
-		return myLastMove;
+				//If we are 5 moves away from having cheated a happy negotiator...
+		if (happiness_counter > 4){
+			happiness_counter = 0;
+			//if they admonished us by disagreeing more than 2/5 times
+			if (happiness_disagree > 2){
+				//we know not to be influened by the happiness rule anymore
+				//because they've discovered we're taking advantage of their
+				//happiness.
+				happiness_influenced = false;
+			}
+			happiness_disagree = 0;
+		}
+		//if we are 6 moves away from capitulating to an angry negotiator
+		if (anger_counter > 5){
+			anger_counter = 0;
+			//if they have not reciprocated at least 2/6 times...
+			if (anger_agree < 3){
+				//we know they are irrationally crazy, and won't return the
+				//favor even if we capitulate. So, ignore the anger rule.
+				anger_influenced = false;
+			}
+			anger_agree = 0;
+		}
+
+		toReturn =  myLastMove;
+		if (next_move != 0 && next_move != toReturn){
+			return next_move;
+		}		
+		return toReturn;
+
 	};
-	self.remember = function(own, other){
+	self.remember = function(own, other, other_mood){
+		next_move = should_change(other_mood);
+
 		myLastMove = own; // remember MISTAKEN move
 		if(other==PD.CHEAT) myLastMove = ((myLastMove==PD.COOPERATE) ? PD.CHEAT : PD.COOPERATE); // switch!
+		if (next_move == PD.CHEAT) happiness_counter++;
+		if (next_move == PD.COOPERATE) anger_counter++;
+
 	};
 }
 
@@ -217,29 +598,78 @@ function Logic_pavlov(){
 function Logic_prober(){
 
 	var self = this;
-
+	var mood = getRandomInt(-10, 6);
+	var happiness_counter = 0;
+	var anger_counter = 0;
+	var anger_agree = 0;
+	var happiness_disagree = 0;
+	var anger_influenced = false;
+	var happiness_influenced = false;
+	var next_move = 0;
+	
 	var moves = [PD.COOPERATE, PD.CHEAT, PD.COOPERATE, PD.COOPERATE];
 	var everCheatedMe = false;
 
 	var otherMove = PD.COOPERATE;
+	self.mood = function(){
+		return mood;
+	}
 	self.play = function(){
+		//If we are 5 moves away from having cheated a happy negotiator...
+		if (happiness_counter > 4){
+			happiness_counter = 0;
+			//if they admonished us by disagreeing more than 2/5 times
+			if (happiness_disagree > 2){
+				//we know not to be influened by the happiness rule anymore
+				//because they've discovered we're taking advantage of their
+				//happiness.
+				happiness_influenced = false;
+			}
+			happiness_disagree = 0;
+		}
+		//if we are 6 moves away from capitulating to an angry negotiator
+		if (anger_counter > 5){
+			anger_counter = 0;
+			//if they have not reciprocated at least 2/6 times...
+			if (anger_agree < 3){
+				//we know they are irrationally crazy, and won't return the
+				//favor even if we capitulate. So, ignore the anger rule.
+				anger_influenced = false;
+			}
+			anger_agree = 0;
+		}
+
+
+		toReturn = 0;
 		if(moves.length>0){
 			// Testing phase
 			var move = moves.shift();
-			return move;
+			toReturn = move;
 		}else{
 			if(everCheatedMe){
-				return otherMove; // TFT
+				toReturn = otherMove; // TFT
 			}else{
-				return PD.CHEAT; // Always Cheat
+				toReturn = PD.CHEAT; // Always Cheat
 			}
 		}
+		
+		if (next_move != 0 && next_move != toReturn){
+			return next_move;
+		}		
+		return toReturn;
+		
 	};
-	self.remember = function(own, other){
+	self.remember = function(own, other, other_mood){
+		next_move = should_change(other_mood);
+
 		if(moves.length>0){
 			if(other==PD.CHEAT) everCheatedMe=true; // Testing phase: ever retaliated?
 		}
 		otherMove = other; // for TFT
+		
+		if (next_move == PD.CHEAT) happiness_counter++;
+		if (next_move == PD.COOPERATE) anger_counter++;
+
 	};
 
 }
